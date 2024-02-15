d4ae85e2d0a2727b635d6e19fefffa5ce02a76bc
Vítor <vitorcanonico@gmail.com>
2024-02-15 11:52:15 -0300

    added react-hook-forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
-import "./App.css"
-import Todo from "./components/Todo"
+import { useForm } from 'react-hook-form';
+import './App.css'
+import Todo from './components/Todo'
 import TodoForm from './components/TodoForm'
 import Search from './components/Search'
 import Filter from './components/Filter'
+import api from './services/api'
 
 function App() {
+
   const [todos, setTodos] = useState([
     {
       id: 1,
@@ -38,6 +41,12 @@ function App() {
   const [filter, setFilter] = useState("All")
   const [sort, setSort] = useState("Asc")
 
+
+
+
+
+
+
   const addTodo = (cep, numero, complemento, prioridade) => {
     const newTodos = [
       ...todos,
@@ -69,8 +78,91 @@ function App() {
     setTodos(newTodos);
   }
 
+
+
+
+
+
+//aqui embaixo é do CEP
+/*   const [input, setInput] = useState('')
+  
+  async function checkCep(){
+    if(input === ''){
+      alert("Preencha algum CEP!")
+      return;
+    }
+    try {
+      const response = await api.get("${input}/json")
+      console.log(response)
+    } catch {
+      
+    }
+  } */
+
+  const {register, handleSubmit, setValue, setFocus} = useForm();
+
+  const onSubmit = (e) => {
+    console.log(e);
+  }
+
+  const checkCep = (e) => {
+    const cep = e.target.value.replace(/\D/g, '');
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+      .then(res => res.json()).then(data => {
+        setValue('address', data.logradouro);
+        setValue('neighborhood', data.bairro);
+        setValue('city', data.localidade);
+        setValue('state', data.uf);
+        setFocus('addressNumber');
+      });
+  }
+
   return (
   <div className='app'>
+
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label>
+        CEP: 
+        <input type='text' {...register("cep")} onBlur={checkCep}/>
+      </label>
+      <label>
+        Rua:
+        <input type="text" {...register("address" )}/>
+      </label>
+      <label>
+        Número:
+        <input type="text" {...register("addressNumber" )}/>
+      </label>
+      <label>
+        Complemento (opcional):
+        <input type="text" {...register("addressExtra" )}/>
+      </label>
+      <label>
+        Bairro:
+        <input type="text" {...register("neighborhood" )}/>
+      </label>
+      <label>
+        Cidade:
+        <input type="text" {...register("city" )}/>
+      </label>
+      <label>
+        Estado:
+        <input type="text" {...register("state" )}/>
+      </label>
+      <select 
+        {...register("prioridade")}
+        //value={prioridade} 
+        //onChange={(e) => setPrioridade(e.target.value)}
+      >
+        <option value=''>Selecione a prioridade</option>
+        <option value='Urgente'>Urgente</option>
+        <option value='Normal'>Normal</option>
+      </select>
+      <button type="submit">Adicionar à lista</button>
+    </form>
+
+
+    <TodoForm addTodo={addTodo}/>
     <h1>Endereços a visitar</h1>
     <Search search={search} setSearch={setSearch} />
     <Filter filter={filter} setFilter={setFilter} setSort={setSort}/>
@@ -100,7 +192,6 @@ function App() {
           />
         ))}
     </div>
-    <TodoForm addTodo={addTodo}/>
   </div>
   )
 }
